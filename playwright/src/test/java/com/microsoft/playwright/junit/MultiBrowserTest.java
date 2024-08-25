14cc442250412e26958efdedc29cd9e937c7086a
DerChris173 <cmattar@gmx.de>
2024-08-25 15:56:39 +0200

    Prototype

--- playwright/src/test/java/com/microsoft/playwright/junit/MultiBrowserTest.java
+++ playwright/src/test/java/com/microsoft/playwright/junit/MultiBrowserTest.java
@@ -1,6 +1,8 @@
 package com.microsoft.playwright.junit;
 
 import com.microsoft.playwright.Browser;
+import com.microsoft.playwright.BrowserContext;
+import com.microsoft.playwright.Page;
 import com.microsoft.playwright.impl.junit.MultiBrowser;
 import org.junit.jupiter.api.Test;
 
@@ -12,6 +14,18 @@ public class MultiBrowserTest {
   public void allBrowsers(Browser browser) {
     // Expected 3 lines, one for each browser.
     logBrowser(browser);
+  }
+
+  @MultiBrowser
+  public void allBrowsers(Page page) {
+    // Expected 3 lines, one for each browser.
+    logBrowser(page.context().browser());
+  }
+
+  @MultiBrowser
+  public void allBrowsers(BrowserContext browserContext) {
+    // Expected 3 lines, one for each browser.
+    logBrowser(browserContext.browser());
   }
 
 
